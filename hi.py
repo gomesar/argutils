b020cc37a94527a7adae19b96f5892b4c94320c9
GomesAR <gomes.bcc@gmail.com>
2016-08-30 00:42:12 -0300

    ...

--- hi.py
+++ hi.py
@@ -4,7 +4,7 @@ import threading
 
 IP          = "0.0.0.0"
 PORT        = 0
-QUIT_CMD    = "!q"
+QUIT_CMD    = "@q"
 
 def ear(s):
     while 1:
@@ -29,7 +29,7 @@ def tongue(s):
             sys.stdout.write("\033[F\033[K")
             sys.stdout.flush()
             if m.startswith(QUIT_CMD):
-                s.sendto(m.replace(QUIT_CMD, '@quitting!    ') , (IP, PORT))
+                s.sendto(m.replace(QUIT_CMD, '@quitting    ') , (IP, PORT))
                 print "[!] Quitting"
                 break;
             else:
