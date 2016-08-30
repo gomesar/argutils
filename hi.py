b8741192cc28c5c6a3c831bcff75edf561337623
GomesAR <gomes.bcc@gmail.com>
2016-08-30 00:30:14 -0300

    Melhor?

--- hi.py
+++ hi.py
@@ -1,3 +1,4 @@
+import sys
 import socket
 import threading
 
@@ -8,34 +9,41 @@ def ear(s):
     while 1:
         try:
             d, a = s.recvfrom(1024)
-            print ">{", d, "}"
+            sys.stdout.write("\033[s\033[K\n:"+d+"\033[u")
+            sys.stdout.flush()
+            #print " {"+d+"} "
+
+            if d.startswith('@quit'):
+                print "[!] DC"
+                break;
         except :
             print "[!] Error"
             break
-
+    s.close()
 
 def tongue(s):
     while 1:
         try:
-            m = raw_input(':')
+            m = raw_input('>')
+            sys.stdout.write("\033[F\033[K")
+            sys.stdout.flush()
             if m.startswith('@quit'):
-                s.sendto(m.replace('@quit', '@Quitting!    ') , (IP, PORT))
+                s.sendto(m.replace('@quit', '@quitting!    ') , (IP, PORT))
                 print "[!] Quitting"
                 break;
             else:
                 s.sendto(m, (IP, PORT))
         except:
             break
-    s.close()
-
 
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(('', PORT))
 
-    ear_h       = threading.Thread(target=ear, args=(s,))
-    ear_h.daemon = True
+    ear_h           = threading.Thread(target=ear, args=(s,))
+    ear_h.daemon    = True
     ear_h.start()
 
     tongue(s)
     
+    s.close()
