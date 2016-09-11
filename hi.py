915ae13e4403d2cc1dbee8855b251c671506eb54
GomesAR <gomes.bcc@gmail.com>
2016-09-11 16:10:40 -0300

    AES suppoprt added. Fixed: Overlap in inbox messages.

--- hi.py
+++ hi.py
@@ -1,24 +1,62 @@
 import sys
 import socket
 import threading
+# Crypt
+import md5
+import base64
+from Crypto.Cipher import AES
+from Crypto import Random
 
 IP          = "0.0.0.0"
 PORT        = 0
 QUIT_CMD    = "@q"
+KEY         = "1t5n0tS4f3"
+# Crypt
+BS      = 16 # Block size
+pad     = lambda s : s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
+unpad   = lambda s : s[:-ord(s[len(s)-1:])]
+trans   = lambda k : md5.new(k).digest()
+
+def printErro(err):
+    print type(err)
+    print err.args
+    print err
+
+def enc(raw, key):
+    try:
+        iv      = Random.new().read(AES.block_size)
+        key_md5 = trans(key)
+        cipher  = AES.new(key_md5, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(pad(raw)) )
+    except Exception as err:
+        printErro(err)
+
+def dec(cph, key):
+    try:
+        cph_d   = base64.b64decode(cph)
+        iv      = cph_d[:BS]
+        key_md5 = trans(key)
+        cipher  = AES.new(key_md5, AES.MODE_CBC, iv)
+        return unpad(cipher.decrypt(cph_d[BS:]) )
+    except Exception as err:
+        printErro(err)
 
 def ear(s):
     while 1:
         try:
             d, a = s.recvfrom(1024)
-            sys.stdout.write("\033[s\033[K\n:"+d+"\033[u")
+            d = dec(d, KEY)
+
+            sys.stdout.write("\033[s\n\033[K:"+d+"\033[u")
             sys.stdout.flush()
             #print " {"+d+"} "
 
             if d.startswith(QUIT_CMD):
                 print "[!] DC"
                 break;
-        except :
-            print "[!] Error"
+        except Except as err:
+            print "[!] Error E"
+            printErro(err)
             break
     s.close()
 
@@ -29,12 +67,16 @@ def tongue(s):
             sys.stdout.write("\033[F\033[K")
             sys.stdout.flush()
             if m.startswith(QUIT_CMD):
-                s.sendto(m.replace(QUIT_CMD, '@quitting    ') , (IP, PORT))
+                m = m.replace(QUIT_CMD, '@quitting    ')
+                s.sendto(enc(m, KEY), (IP, PORT))
                 print "[!] Quitting"
+                s.close()
                 break;
             else:
-                s.sendto(m, (IP, PORT))
-        except:
+                s.sendto(enc(m, KEY), (IP, PORT))
+        except Exception as err:
+            #print "[!] Error T"
+            #printErro(err)
             break
 
 if __name__ == "__main__":
@@ -46,5 +88,4 @@ if __name__ == "__main__":
     ear_h.start()
 
     tongue(s)
-    
-    s.close()
+
