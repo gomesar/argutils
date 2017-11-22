a635ccaa10692d12a6635067c0ae532dc9c37831
ra191071 <agomes@lasca.ic.unicamp.br>
2017-11-22 17:19:16 -0200

    net checker v0.5

--- is_net.sh
+++ is_net.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+start=$(date +%H:%M:%S)
 ping -c 1 8.8.8.8 &>/dev/null
 while [ $? -ne 0 ];
 do
 	time=$(date +%H:%M:%S)
-	echo "$time: Net failed."
+	echo "[$time] Net failed."
 	sleep 5
 
 	ping -c 1 8.8.8.8 &>/dev/null
@@ -14,19 +15,22 @@ ping -c 1 google.com &>/dev/null
 while [ $? -ne 0 ]
 do
 	time=$(date +%H:%M:%S)
-	echo "$time: DNS failed."
+	echo "[$time] DNS failed."
 	sleep 5
 
 	ping -c 1 google.com &>/dev/null
 done
+end=$(date +%H:%M:%S)
 
-echo "  _.._..,_,_   Welcome"
-echo " (          )    back!"
-echo "  ]~,\"-.-~~["
-echo ".=])' (;  (["
-echo "| ]:: '    ["
-echo "'=]): .)  (["
-echo "  |:: '    |"
-echo "   ~~----~~"
+echo "[!] ($start -> $end)"
+echo ""
+echo "    _.._..,_,_   Welcome"
+echo "   (          )    back!"
+echo "    ]~,\"-.-~~["
+echo "  .=])' (;  (["
+echo "  | ]:: '    ["
+echo "  '=]): .)  (["
+echo "    |:: '    |"
+echo "     ~~----~~ "
 # ASCII figure created by: Paul Martin Howard
 
