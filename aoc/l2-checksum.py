17d15553697f7d3c0e2c606d3707c054dd5d738a
ra191071 <agomes@lasca.ic.unicamp.br>
2017-12-05 14:48:16 -0200

    l2 em python

---
+++ aoc/l2-checksum.py
@@ -0,0 +1,21 @@
+def do_cksum(file_name):
+    li = list()
+    sum = 0
+
+    with open(file_name, "r") as csv:
+        for line in csv:
+            line = line.replace("\n","").split("\t")
+            line = [int(x) for x in line]
+
+            #print(line)
+            mi = int(min(line))
+            ma = int(max(line))
+            tt = ma - mi
+            #print( "Max - Min = {} - {} = {}".format(ma, mi, tt) )
+            sum += tt
+        print(sum)
+
+
+if __name__ == "__main__":
+    file_name = "checksum.csv"
+    do_cksum(file_name)
