2cd2cd49edf4d3741ba68fde24956eef10e13250
ra191071 <agomes@lasca.ic.unicamp.br>
2018-02-26 17:20:02 -0300

    change dir and ls

---
+++ home/tools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# @GomesAR functions
+# tools for terminal
+# 2018-02 MIT License
+
+
+function cdl() {	
+	cd $1
+	curr=`pwd`
+	printf "\t\033[0;31mcd to: ${curr}\033[0m\n"
+	ls -lhF
+}
